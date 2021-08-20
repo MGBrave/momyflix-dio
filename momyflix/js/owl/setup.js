3c43a12fed0ee4483e7493c5aeaa335046b61607
Marta Geraldo <60014891+martageraldo@users.noreply.github.com>
2021-08-20 11:44:00 -0300

    Desafio Dio
    #DigitalInovationOne 
    #JavaccriptGameDeveloper

---
+++ momyflix/js/owl/setup.js
@@ -0,0 +1,17 @@
+
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:9,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[3000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
